00e80f8217994c44174c8a3ee2cf43341719c366
awmleer <awmleer@sparker.xyz>
2018-12-09 19:55:07 +0800

    login request done

--- zjuwlan.js
+++ zjuwlan.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-let program = require('commander')
+const program = require('commander')
+const request = require('request')
+
 
 program
   .version('0.1.0', '-v, --version')
@@ -10,7 +12,38 @@ program
   .option('-p, --password [password]', 'use the given password')
   .option('-u, --username [username]', 'use the given username')
   .action(function (cmd) {
-    console.log(`login ${cmd.username} ${cmd.password}`)
+    // console.log(`login ${cmd.username} ${cmd.password}`)
+    const {username, password} = cmd
+    request.post({
+      url: 'http://net.zju.edu.cn:802/include/auth_action.php',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/plain','Host':'net.zju.edu.cn:802',
+        'Origin':'net.zju.edu.cn:802',
+        'Referer':'http://net.zju.edu.cn:802/srun_portal_pc.php?ac_id=1&&ac_id=3'
+      },
+      form: {
+        'action': 'login',
+        'username': username,
+        'password': password,
+        'ac_id': '3',
+        'type': '1',
+        'is_ldap': '1',
+        'local_auth': '1',
+        'user_ip':'',
+        'nas_ip':'',
+        'user_mac':'',
+        'save_me':'1',
+        'ajax':'1'
+      }
+    }, (error, response, body) => {
+      if (error) {
+        console.error('login failed')
+        return
+      }
+      console.log(response.body)
+      console.log(response.body.includes('login_ok'))
+    })
   })
 
 program
